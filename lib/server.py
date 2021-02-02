6fb9d85fcd4fafdd04086ca72cf243915f567fd7
Dhruv Patel <35431701+dhruvp97@users.noreply.github.com>
2021-02-01 21:22:00 -0500

    Add files via upload

--- lib/server.py
+++ lib/server.py
@@ -1,14 +1,42 @@
 from flask import Flask
+from flask import request, jsonify
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+simple_lib = [
+    {
+        'id': 0,
+        'title': 'AAAAA'
+    },
+    {
+        'id': 1,
+        'title': 'BBBBB'
+    },
+    {
+        'id': 2,
+        'title': 'CCCCC'
+    }
+]
 
 @app.route("/")
 def home():
     return "Welcome to ECE231!"
 
-@app.route("/hello")
-def simple():
-    return "Hello, Pratyush!"
+@app.route("/a", methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error"
+    results = []
+
+    for entry in simple_lib:
+        if entry['id'] == id:
+            results.append(entry)
+
+    return jsonify(results)
 
     
 if __name__ == "__main__":
